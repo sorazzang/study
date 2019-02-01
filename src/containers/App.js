62e24663f10a68e031ff29c5d3b7b15313b2e812
sora <soralee2@grovesoft.net>
2019-02-01 10:22:26 +0900

    single-counter project commit

---
+++ src/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Counter from '../components/Counter';
+import CounterContainer from '../containers/CounterContainer';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <CounterContainer>
+                    <Counter />
+                </CounterContainer>
+            </div>
+        );
+    }
+}
+
+export default App;
